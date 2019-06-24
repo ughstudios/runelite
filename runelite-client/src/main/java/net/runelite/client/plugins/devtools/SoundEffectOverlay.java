Wea1thRS/runelite-main

Add sounds to devtools

runelite-client/src/main/java/net/runelite/client/plugins/devtools/SoundEffectOverlay.java
@@ -1,0 +1,156 @@
+/*
+ * Copyright (c) 2018, WooxSolo <https://github.com/WooxSolo>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.devtools;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.Player;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.AreaSoundEffectPlayed;
+import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+
+public class SoundEffectOverlay extends Overlay
+{
+	private final static int MAX_LINES = 16;
+	private final static Color COLOR_SOUND_EFFECT = Color.WHITE;
+	private final static Color COLOR_AREA_SOUND_EFFECT = Color.YELLOW;
+	private final static Color COLOR_SILENT_SOUND_EFFECT = Color.GRAY;
+
+	private final Client client;
+	private final DevToolsPlugin plugin;
+	private final PanelComponent panelComponent = new PanelComponent();
+
+	@Inject
+	SoundEffectOverlay(Client client, DevToolsPlugin plugin, EventBus eventBus)
+	{
+		this.client = client;
+		this.plugin = plugin;
+		panelComponent.setPreferredSize(new Dimension(200, 0));
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left("Sound Effects")
+			.leftColor(Color.CYAN)
+			.build());
+		setPosition(OverlayPosition.TOP_LEFT);
+
+		eventBus.register(this);
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		if (!plugin.isToggleSoundEffects())
+		{
+			return null;
+		}
+
+		return panelComponent.render(graphics);
+	}
+
+	@Subscribe
+	public void onSoundEffectPlayed(SoundEffectPlayed event)
+	{
+		if (!plugin.isToggleSoundEffects())
+		{
+			return;
+		}
+
+		String text =
+			"Id: " + event.getSoundId() +
+			" - D: " + event.getDelay();
+
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left(text)
+			.leftColor(COLOR_SOUND_EFFECT)
+			.build());
+
+		checkMaxLines();
+	}
+
+	@Subscribe
+	public void onAreaSoundEffectPlayed(AreaSoundEffectPlayed event)
+	{
+		if (!plugin.isToggleSoundEffects())
+		{
+			return;
+		}
+
+		Color textColor = COLOR_AREA_SOUND_EFFECT;
+
+		// Check if the player is within range to hear the sound
+		Player localPlayer = client.getLocalPlayer();
+		if (localPlayer != null)
+		{
+			LocalPoint lp = localPlayer.getLocalLocation();
+			if (lp != null)
+			{
+				int sceneX = lp.getSceneX();
+				int sceneY = lp.getSceneY();
+				int distance = Math.abs(sceneX - event.getSceneX()) + Math.abs(sceneY - event.getSceneY());
+				if (distance > event.getRange())
+				{
+					textColor = COLOR_SILENT_SOUND_EFFECT;
+				}
+			}
+		}
+
+		String text =
+			"Id: " + event.getSoundId() +
+			" - L: " + event.getSceneX() + "," + event.getSceneY() +
+			" - R: " + event.getRange() +
+			" - D: " + event.getDelay();
+
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left(text)
+			.leftColor(textColor)
+			.build());
+
+		checkMaxLines();
+	}
+
+	private void checkMaxLines()
+	{
+		while (panelComponent.getChildren().size() > MAX_LINES)
+		{
+			panelComponent.getChildren().remove(1);
+		}
+	}
+
+	void clear()
+	{
+		while (panelComponent.getChildren().size() > 1)
+		{
+			panelComponent.getChildren().remove(1);
+		}
+	}
+}

runelite-mixins/src/main/java/net/runelite/mixins/SoundEffectMixin.java
@@ -1,0 +1,120 @@
+/*
+ * Copyright (c) 2018, SomeoneWithAnInternetConnection
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.mixins;
+
+import net.runelite.api.events.AreaSoundEffectPlayed;
+import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.api.mixins.FieldHook;
+import net.runelite.api.mixins.Inject;
+import net.runelite.api.mixins.Mixin;
+import net.runelite.api.mixins.Shadow;
+import net.runelite.rs.api.RSClient;
+import net.runelite.rs.api.RSSoundEffect;
+
+@Mixin(RSClient.class)
+public abstract class SoundEffectMixin implements RSClient
+{
+	@Shadow("clientInstance")
+	private static RSClient client;
+
+	@Inject
+	private static int lastSoundEffectCount;
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id)
+	{
+		playSoundEffect(id, 0, 0, 0, 0);
+	}
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id, int x, int y, int range)
+	{
+		playSoundEffect(id, x, y, range, 0);
+	}
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id, int x, int y, int range, int delay)
+	{
+		int position = ((x & 255) << 16) + ((y & 255) << 8) + (range & 255);
+
+		int[] queuedSoundEffectIDs = getQueuedSoundEffectIDs();
+		int[] unknownSoundValues1 = getUnknownSoundValues1();
+		int[] queuedSoundEffectDelays = getQueuedSoundEffectDelays();
+		RSSoundEffect[] audioEffects = getAudioEffects();
+		int[] soundLocations = getSoundLocations();
+		int queuedSoundEffectCount = getQueuedSoundEffectCount();
+
+		queuedSoundEffectIDs[queuedSoundEffectCount] = id;
+		unknownSoundValues1[queuedSoundEffectCount] = 1;
+		queuedSoundEffectDelays[queuedSoundEffectCount] = delay;
+		audioEffects[queuedSoundEffectCount] = null;
+		soundLocations[queuedSoundEffectCount] = position;
+
+		setQueuedSoundEffectCount(queuedSoundEffectCount + 1);
+	}
+
+	@FieldHook("queuedSoundEffectCount")
+	@Inject
+	public static void queuedSoundEffectCountChanged(int idx)
+	{
+		int soundCount = client.getQueuedSoundEffectCount();
+		if (soundCount == lastSoundEffectCount + 1)
+		{
+			int soundIndex = soundCount - 1;
+			int packedLocation = client.getSoundLocations()[soundIndex];
+
+			if (packedLocation == 0)
+			{
+				// Regular sound effect
+
+				SoundEffectPlayed event = new SoundEffectPlayed();
+				event.setSoundId(client.getQueuedSoundEffectIDs()[soundIndex]);
+				event.setDelay(client.getQueuedSoundEffectDelays()[soundIndex]);
+				client.getCallbacks().post(event);
+			}
+			else
+			{
+				// Area sound effect
+
+				int x = (packedLocation >> 16) & 0xFF;
+				int y = (packedLocation >> 8) & 0xFF;
+				int range = (packedLocation) & 0xFF;
+
+				AreaSoundEffectPlayed event = new AreaSoundEffectPlayed();
+				event.setSoundId(client.getQueuedSoundEffectIDs()[soundIndex]);
+				event.setSceneX(x);
+				event.setSceneY(y);
+				event.setRange(range);
+				event.setDelay(client.getQueuedSoundEffectDelays()[soundIndex]);
+				client.getCallbacks().post(event);
+			}
+		}
+
+		lastSoundEffectCount = soundCount;
+	}
+}

ughstudios/runelite

runelite-client/src/main/java/net/runelite/client/plugins/devtools/SoundEffectOverlay.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (c) 2018, WooxSolo <https://github.com/WooxSolo>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.devtools;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.Player;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.AreaSoundEffectPlayed;
+import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+
+class SoundEffectOverlay extends Overlay
+{
+	private final static int MAX_LINES = 16;
+	private final static Color COLOR_SOUND_EFFECT = Color.WHITE;
+	private final static Color COLOR_AREA_SOUND_EFFECT = Color.YELLOW;
+	private final static Color COLOR_SILENT_SOUND_EFFECT = Color.GRAY;
+
+	private final Client client;
+	private final DevToolsPlugin plugin;
+	private final PanelComponent panelComponent = new PanelComponent();
+
+	@Inject
+	SoundEffectOverlay(Client client, DevToolsPlugin plugin)
+	{
+		this.client = client;
+		this.plugin = plugin;
+		panelComponent.setPreferredSize(new Dimension(200, 0));
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left("Sound Effects")
+			.leftColor(Color.CYAN)
+			.build());
+		setPosition(OverlayPosition.TOP_LEFT);
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		if (!plugin.getSoundEffects().isActive())
+		{
+			return null;
+		}
+
+		return panelComponent.render(graphics);
+	}
+
+	@Subscribe
+	public void onSoundEffectPlayed(SoundEffectPlayed event)
+	{
+		if (!plugin.getSoundEffects().isActive())
+		{
+			return;
+		}
+
+		String text =
+			"Id: " + event.getSoundId() +
+			" - D: " + event.getDelay();
+
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left(text)
+			.leftColor(COLOR_SOUND_EFFECT)
+			.build());
+
+		checkMaxLines();
+	}
+
+	@Subscribe
+	public void onAreaSoundEffectPlayed(AreaSoundEffectPlayed event)
+	{
+		if (!plugin.getSoundEffects().isActive())
+		{
+			return;
+		}
+
+		Color textColor = COLOR_AREA_SOUND_EFFECT;
+
+		// Check if the player is within range to hear the sound
+		Player localPlayer = client.getLocalPlayer();
+		if (localPlayer != null)
+		{
+			LocalPoint lp = localPlayer.getLocalLocation();
+			if (lp != null)
+			{
+				int sceneX = lp.getSceneX();
+				int sceneY = lp.getSceneY();
+				int distance = Math.abs(sceneX - event.getSceneX()) + Math.abs(sceneY - event.getSceneY());
+				if (distance > event.getRange())
+				{
+					textColor = COLOR_SILENT_SOUND_EFFECT;
+				}
+			}
+		}
+
+		String text =
+			"Id: " + event.getSoundId() +
+			" - L: " + event.getSceneX() + "," + event.getSceneY() +
+			" - R: " + event.getRange() +
+			" - D: " + event.getDelay();
+
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left(text)
+			.leftColor(textColor)
+			.build());
+
+		checkMaxLines();
+	}
+
+	private void checkMaxLines()
+	{
+		while (panelComponent.getChildren().size() > MAX_LINES)
+		{
+			panelComponent.getChildren().remove(1);
+		}
+	}
+}

runelite-mixins/src/main/java/net/runelite/mixins/SoundEffectMixin.java
@@ -1,0 +1,120 @@
+/*
+ * Copyright (c) 2018, SomeoneWithAnInternetConnection
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.mixins;
+
+import net.runelite.api.events.AreaSoundEffectPlayed;
+import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.api.mixins.FieldHook;
+import net.runelite.api.mixins.Inject;
+import net.runelite.api.mixins.Mixin;
+import net.runelite.api.mixins.Shadow;
+import net.runelite.rs.api.RSClient;
+import net.runelite.rs.api.RSSoundEffect;
+
+@Mixin(RSClient.class)
+public abstract class SoundEffectMixin implements RSClient
+{
+	@Shadow("clientInstance")
+	private static RSClient client;
+
+	@Inject
+	private static int lastSoundEffectCount;
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id)
+	{
+		playSoundEffect(id, 0, 0, 0, 0);
+	}
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id, int x, int y, int range)
+	{
+		playSoundEffect(id, x, y, range, 0);
+	}
+
+	@Inject
+	@Override
+	public void playSoundEffect(int id, int x, int y, int range, int delay)
+	{
+		int position = ((x & 255) << 16) + ((y & 255) << 8) + (range & 255);
+
+		int[] queuedSoundEffectIDs = getQueuedSoundEffectIDs();
+		int[] queuedSoundEffectLoops = getQueuedSoundEffectLoops();
+		int[] queuedSoundEffectDelays = getQueuedSoundEffectDelays();
+		RSSoundEffect[] audioEffects = getAudioEffects();
+		int[] soundLocations = getSoundLocations();
+		int queuedSoundEffectCount = getQueuedSoundEffectCount();
+
+		queuedSoundEffectIDs[queuedSoundEffectCount] = id;
+		queuedSoundEffectLoops[queuedSoundEffectCount] = 1;
+		queuedSoundEffectDelays[queuedSoundEffectCount] = delay;
+		audioEffects[queuedSoundEffectCount] = null;
+		soundLocations[queuedSoundEffectCount] = position;
+
+		setQueuedSoundEffectCount(queuedSoundEffectCount + 1);
+	}
+
+	@FieldHook("queuedSoundEffectCount")
+	@Inject
+	public static void queuedSoundEffectCountChanged(int idx)
+	{
+		int soundCount = client.getQueuedSoundEffectCount();
+		if (soundCount == lastSoundEffectCount + 1)
+		{
+			int soundIndex = soundCount - 1;
+			int packedLocation = client.getSoundLocations()[soundIndex];
+
+			if (packedLocation == 0)
+			{
+				// Regular sound effect
+
+				SoundEffectPlayed event = new SoundEffectPlayed();
+				event.setSoundId(client.getQueuedSoundEffectIDs()[soundIndex]);
+				event.setDelay(client.getQueuedSoundEffectDelays()[soundIndex]);
+				client.getCallbacks().post(event);
+			}
+			else
+			{
+				// Area sound effect
+
+				int x = (packedLocation >> 16) & 0xFF;
+				int y = (packedLocation >> 8) & 0xFF;
+				int range = (packedLocation) & 0xFF;
+
+				AreaSoundEffectPlayed event = new AreaSoundEffectPlayed();
+				event.setSoundId(client.getQueuedSoundEffectIDs()[soundIndex]);
+				event.setSceneX(x);
+				event.setSceneY(y);
+				event.setRange(range);
+				event.setDelay(client.getQueuedSoundEffectDelays()[soundIndex]);
+				client.getCallbacks().post(event);
+			}
+		}
+
+		lastSoundEffectCount = soundCount;
+	}
+}