arbahb/ulu-p

timers buffs: add smouldering gland
The interval of 4 is based on struct 966, param 1541 (script 5923)

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -635,6 +635,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarTimer(SMOULDERING_HEART, event.getValue(), i -> i * 25);
 		}
 
+		if (event.getVarbitId() == Varbits.SMOULDERING_GLAND && config.showPrayerEnhance())
+		{
+			updateVarTimer(SMOULDERING_GLAND, event.getValue(), i -> i * 4);
+		}
+
 		if (event.getVarbitId() == Varbits.BUFF_GOADING_POTION && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -688,6 +693,7 @@ public class TimersAndBuffsPlugin extends Plugin
 		if (!config.showPrayerEnhance())
 		{
 			removeGameTimer(PRAYER_ENHANCE);
+			removeGameTimer(SMOULDERING_GLAND);
 		}
 
 		if (!config.showDivine())

ughstudios/runelite

runelite-client/src/main/java/net/runelite/client/plugins/timersandbuffs/TimersAndBuffsPlugin.java
@@ -633,6 +633,11 @@ public class TimersAndBuffsPlugin extends Plugin
 			updateVarTimer(SMOULDERING_HEART, event.getValue(), i -> i * 25);
 		}
 
+		if (event.getVarbitId() == VarbitID.WGS_SMOULDERING_GLAND_TIMER && config.showTormentedDemonBuffs())
+		{
+			updateVarTimer(SMOULDERING_GLAND, event.getValue(), i -> i * 4);
+		}
+
 		if (event.getVarbitId() == VarbitID.GOADING_POTION_TIMER && config.showGoading())
 		{
 			updateVarTimer(GOADING, event.getValue(), i -> i * 6);
@@ -690,6 +695,7 @@ public class TimersAndBuffsPlugin extends Plugin
 		{
 			removeVarCounter(STONE_OF_JAS_EMPOWERMENT);
 			removeGameTimer(SMOULDERING_HEART);
+			removeGameTimer(SMOULDERING_GLAND);
 		}
 
 		if (!config.showPrayerEnhance())